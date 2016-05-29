46512d436cf8eae8125de9ebc5fb0056ae4da968
WenChi <u10416019@go.utaipei.edu.tw>
2016-05-29 20:45:36 +0800

    Animation of the class
    I create two buttons to let the clock stop and start, first I create a HBox to input the stop and start buttons, and then I create a pane to input the clock pane and the button pane, after that, I create the animation of the stop and the start buttons to let the clock stop and start.

--- ClockAnimation.java
+++ ClockAnimation.java
@@ -17,6 +17,22 @@ public class ClockAnimation extends Application {
   public void start(Stage primaryStage) {
     ClockPane clock = new ClockPane(); // Create a clock
 	
+	//create stop and start button to let the time stop or start
+	Button stop = new Button("stop");
+	Button start = new Button("start");
+	start.setPrefSize(120,30);
+	stop.setPrefSize(120,30);
+	
+	//create a HBox to input two button
+	HBox hBox1 = new HBox(10);
+	hBox1.getChildren().addAll(start, stop);
+	
+	//create a pane and input the clock and button pane in it
+	BorderPane pane = new BorderPane();
+	pane.setCenter(clock);
+	pane.setBottom(hBox1);
+	
+	
     // Create a handler for animation
     EventHandler<ActionEvent> eventHandler = e -> {
       clock.setCurrentTime(); // Set a new clock time
@@ -29,6 +45,17 @@ public class ClockAnimation extends Application {
     animation.setCycleCount(Timeline.INDEFINITE);
     animation.play(); // Start animation
     
+    
+    //create the animation of the stop button then stop the time
+    stop.setOnAction((ActionEvent e) -> {
+    	animation.stop();
+	});
+    
+    //create the animation of the start button then start the time
+    start.setOnAction((ActionEvent e) -> {
+    	animation.play();
+	});
+	
     
     // Create a scene and place it in the stage
     Scene scene = new Scene(pane, 250, 250);
